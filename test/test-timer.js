1db19c5874e7a242ae9dbba227112da49caea4a7
ctoesca <christophe.toesca@gmail.com>
2019-01-06 16:37:43 +0100

    v4

---
+++ test/test-timer.js
@@ -0,0 +1,45 @@
+
+const assert = require('assert');
+const utils = require('../dist/utils')
+const Promise = require('bluebird');
+
+describe('Timer', function() 
+{
+	var timer = new utils.Timer({delay: 1000})
+	
+  	describe('timer event after 1 sec', function() {
+
+	    it('should emit timer event', function( done ) {
+
+	    	var timerEvent = function(){
+  				timer.removeListener(utils.Timer.ON_TIMER, timerEvent)
+  				done()
+  			}
+
+	    	timer.on(utils.Timer.ON_TIMER, timerEvent)
+  			timer.start()
+
+	    });
+  	});
+
+  	  
+  	describe('timer stopper: no event', function() {
+	    it('should not emit timer event', function( done ) {
+	    	this.timeout(5000);
+	    	
+
+	    	var timerEvent = function(){
+  				timer.removeListener(utils.Timer.ON_TIMER, timerEvent)
+  				done("Timer send event but is stopped")
+  			}
+	    	timer.on(utils.Timer.ON_TIMER, timerEvent)
+	    	timer.stop()
+
+	    	setTimeout( () => {
+	    		done()
+	    	}, 2000)
+
+	    });
+  	});
+
+});
