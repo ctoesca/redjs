36e8e338fd118f4abd87bf97d76ff60d693ad896
ctoesca <christophe.toesca@gmail.com>
2019-02-16 16:04:30 +0100

    tests

--- test/test-app.js
+++ test/test-app.js
@@ -27,55 +27,43 @@ global.getRedis = function(){
 	return new Redis(global.PORT, global.HOST)	
 }
 
-if (cluster.isMaster)
-{
-	cluster.fork();
 
-	cluster.on('fork', (worker) => {
-		console.log("************ FORK WORKER **************")
-	})
+/* WORKER */
+var redjsServer = new RedjsServer(global.PORT, global.HOST, {})
+redjsServer.start()
 
-	cluster.on('exit', (worker, code, signal) => {
-		process.exit(code)
-	})
+console.log("************ WORKER CREATED **************")
 
-	
+var testDir = __dirname
 
-}else 
-{
-	/* WORKER */
-	var redjsServer = new RedjsServer(global.PORT, global.HOST, {})
-	redjsServer.start()
+var mocha = new Mocha({
+    //reporter: 'spec'
+    reporter: 'mochawesome',
+    reporterOptions: {
+    	reportDir: __dirname+'/mocha-report'
+      	//reportName: 'report',
+    }
+});
 
-	console.log("************ WORKER CREATED **************")
+fs.readdirSync(testDir).filter(function(file) {
+    return (file.substr(-3) === '.js') && (file != 'test-app.js');
 
-	var testDir = __dirname
+}).forEach(function(file) {
+	console.log("ADD FILE "+file)
+    mocha.addFile(
+        path.join(testDir, file)
+    );
+});
 
-	var mocha = new Mocha({
-	    //reporter: 'spec'
-	    reporter: 'mochawesome'
-	});
+// Run the tests.
+// Instantiate a Mocha instance.
+mocha.run(function(failures) {
 
-	fs.readdirSync(testDir).filter(function(file) {
-	    return (file.substr(-3) === '.js') && (file != 'test-app.js');
+  	var exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
+  	console.log("EXITCODE="+exitCode)	
+	process.exitCode = exitCode
+	process.exit()
+});
 
-	}).forEach(function(file) {
-		console.log("ADD FILE "+file)
-	    mocha.addFile(
-	        path.join(testDir, file)
-	    );
-	});
-
-	// Run the tests.
-	// Instantiate a Mocha instance.
-	mocha.run(function(failures) {
-
-	  	var exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
-	  	console.log("EXITCODE="+exitCode)	
-		process.exitCode = exitCode
-		process.exit()
-	});
-
-}
 
 
